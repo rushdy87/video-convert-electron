8c7ad883b352a78fc019a13150daceec1ab9be63
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-02-03 22:51:40 +0300

    Detecting Conversion Progress

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ ipcMain.on('conversion:start', (event, videos) => {
 
     ffmpeg(video.path)
       .output(outputPath)
+      .on('progress', ({ timemark }) => {
+        mainWindow.webContents.send('conversion:progress', {
+          video,
+          timemark,
+        });
+      })
       .on('end', () => {
         mainWindow.webContents.send('conversion:end', { video, outputPath });
       })
